aa7b12740135e2c3e494553f4edb182c5494f4b8
Duc Anh Nguyen <53148787+DepTrai95@users.noreply.github.com>
2023-12-20 16:22:22 +0100

    [FEATURE] add schedule list functionality + base styling

---
+++ src/app/components/ScheduleList.tsx
@@ -0,0 +1,88 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { GET } from "../api/schedule/route";
+import { BeatLoader } from "react-spinners";
+
+interface BusStop {
+  route: string;
+  stops: {
+    name: string;
+    time: string;
+  }[];
+}
+
+const Schedule: React.FC = () => {
+  const [scheduleData, setScheduleData] = useState<BusStop[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchScheduleData = async () => {
+      try {
+        setLoading(true);
+        const response = await GET();
+        const data: BusStop[] = await response.json();
+        setScheduleData(data);
+      } catch (error) {
+        console.error("Error fetching schedule data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchScheduleData();
+  }, []);
+
+  return (
+    <div className="content-area">
+      <div className="inner">
+        {loading ? (
+          <div className="text-center">
+            <BeatLoader color="#000000" size={10} margin={3} />
+          </div>
+        ) : (
+          scheduleData.length > 0 && (
+            <div>
+              <h2>Busfahrplan</h2>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Route</th>
+                    <th>Haltestelle</th>
+                    <th>Abfahrtszeit</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {scheduleData.map((route, index) => (
+                    <React.Fragment key={`${route.route}-${index}`}>
+                      {route.stops.map((stop, stopIndex) => (
+                        <tr key={`${stop.name}-${stopIndex}`}>
+                          {stopIndex === 0 && (
+                            <td rowSpan={route.stops.length}>{route.route}</td>
+                          )}
+                          <td>{stop.name}</td>
+                          <td>{stop.time}</td>
+                        </tr>
+                      ))}
+                      {route.stops.length === 0 && (
+                        <tr>
+                          <td>{route.route}</td>
+                          <td colSpan={2}>
+                            <span className="text-center">
+                              Keine verfügbaren Informationen
+                            </span>
+                          </td>
+                        </tr>
+                      )}
+                    </React.Fragment>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Schedule;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,31 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import Button from "./components/Button";
+import Schedule from "./components/ScheduleList";
 import Filter from "./components/Filter";
-import Schedule from "./components/Schedule";
 import StopDetails from "./components/StopDetails";
 
 export default function Home() {
+  const [selectedButton, setSelectedButton] = useState<string | null>(null);
+
+  const handleButtonClick = (buttonName: string) => {
+    setSelectedButton(buttonName);
+  };
+
   return (
     <main>
-      <div>
-        <h1>Busfahrplan</h1>
-        <Filter></Filter>
-        <Schedule></Schedule>
-        <StopDetails></StopDetails>
+      <h1 className="text-center">Bus Schedule Coding Task</h1>
+      <div className="content-area">
+        <div className="inner">
+          <Button
+            className="btn--primary"
+            label="Lade Busfahrplan"
+            onClick={() => handleButtonClick("schedule")}
+          />
+        </div>
       </div>
+      
+      {selectedButton === "schedule" && <Schedule />}
     </main>
   );
 }
